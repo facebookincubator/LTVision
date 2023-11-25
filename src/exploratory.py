3d05c14e3a2028830576ce6a8ae5b3ea7a4f79bb
Mikael Wallgren <mikaelwallgren@gmail.com>
2023-11-25 23:29:54 +0100

    Adding docstring
    Adding docstring for module

--- src/exploratory.py
+++ src/exploratory.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+"""Module providing a class for initial analysis"""
+
 from typing import List
 import matplotlib.pyplot as plt
 import numpy as np
