583aa82b0ef5f24b44c05391eb0ddbfc1e0ef744
Raphael Tamaki <raphaeltamaki@fb.com>
2023-12-08 12:06:55 +0100

    updated minimum configurations for Pages

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://facebookexperimental.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/LTVision/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'facebookexperimental', // Usually your GitHub org/user name.
+  projectName: 'LTVision', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
