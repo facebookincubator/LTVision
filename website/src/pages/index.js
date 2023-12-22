c10409164f84817e164c3b1e29544a25e854255f
Raphael Tamaki <raphaeltamaki@fb.com>
2023-12-22 15:42:40 +0100

    Feature: Added central content of front page
    Added central content of frontal page. Format follows GeoLift's.
    
    TODO: Add image or video to reflect GeoLift's

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -28,6 +28,40 @@ function HomepageHeader() {
   );
 }
 
+
+function HomePageCentralContent() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <div className="padding-vert--xl">
+      <div className="container">
+        <div className="row">
+          <div className={clsx('col col--6', styles.descriptionSection)}>
+            <h2>A foundational Open-Source Library for Lifetime Value (LTV) prediction</h2>
+              <p className={styles.descriptionSectionText}>
+                Best-in-class businesses are taking action and learning how they can incorporate their first-party data into their strategy now. 
+                Businesses who have right customer data strategy and infrastructure in place can start their journey to leverage exciting opportunity 
+                from data science methods which help predict future (long term) value of customer, referred as pLTV (predicted lifetime value) of customer. <br></br>                
+                A pLTV-focused approach to marketing  offers a significant advantage to  businesses over relying on short-term return on ad spend (ROAS) 
+              and cost tactics in terms of building a profitable business.
+              </p>
+          </div>
+          <div className="col col--6">
+            <iframe
+              width="100%"
+              height="400"
+              src="https://www.youtube.com/embed/ogLGvNOpCmg"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -36,6 +70,7 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
+        <HomePageCentralContent />
         <HomepageFeatures />
       </main>
     </Layout>
