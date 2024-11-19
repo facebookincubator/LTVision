33660f5904d3fe1bc41673706304136d9648ffde
Omer Kayabasi <omerkayabasi@meta.com>
2024-11-19 15:55:21 +0300

    feat: add copyright headers

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,3 +1,6 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
 // @ts-check
 // `@type` JSDoc annotations allow editor autocompletion and type checking
 // (when paired with `@ts-check`).
