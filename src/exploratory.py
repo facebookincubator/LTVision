3eb19a03c4a20bff4c3220d3c6829da4904e26be
Omer Kayabasi <omerkayabasi@meta.com>
2024-11-25 13:22:25 +0000

    fix: :zap: rename variable, bump default precision

--- src/exploratory.py
+++ src/exploratory.py
@@ -39,7 +39,7 @@ class LTVexploratory:
         event_name_col: str = "event_name",
         value_col: str = "purchase_value",
         segment_feature_cols: List[str] = None,
-        decimals: int = 1,
+        roundingPrecision: int = 5,
     ):
         self.data_customers = data_customers
         self.data_events = data_events
@@ -47,7 +47,7 @@ class LTVexploratory:
         self._period_for_ltv = 7 * 10
         self.graph = Graph()
         self.interactive_chart = InteractiveChart()
-        self.decimals = decimals
+        self.roundingPrecision = roundingPrecision
 
         # store information about the columns of the dataframes
         self.uuid_col = uuid_col
@@ -784,7 +784,7 @@ class LTVexploratory:
             title=f"Purchaser Flow Between an early point in time - {early_limit} days and a future point in time - {days_limit} days",
         )
 
-        return fig, visualization_data.round(self.decimals)
+        return fig, visualization_data.round(self.roundingPrecision)
 
     def _get_upper_limit_ltv(
         self, users_flow_df: pd.DataFrame, is_mobile: bool
@@ -891,7 +891,7 @@ class LTVexploratory:
         """
         print(output_txt)
 
-        return data.round(self.decimals)
+        return data.round(self.roundingPrecision)
 
     def download_data(
         self,
